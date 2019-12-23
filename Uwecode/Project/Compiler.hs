640184ae421f152ddb94b1adc9f35daec2cbd62f
Sam Alws <sam.alws@gmail.com>
2019-12-23 17:42:27 -0600

    util file

--- Uwecode/Project/Compiler.hs
+++ Uwecode/Project/Compiler.hs
@@ -6,66 +6,36 @@ import Uwecode.CodeReader
 import Uwecode.IO
 import Uwecode.StdIOs
 import Uwecode.Project.Project
+import Uwecode.Project.Util
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.Maybe
 import System.IO
-import System.Process
-import System.Directory
-import Data.Maybe
 import Data.List
-import System.FilePath
 
-showStrList :: [String] -> String
-showStrList s = '[' : (concat $ intersperse ", " s) ++ "]"
-
-makeImportStatements :: [(String, String)] -> String
-makeImportStatements = concat . map helper where
-    helper (i, "") = "import " ++ i ++ "\n"
-    helper (i, j)  = "import qualified " ++ i ++ " as " ++ j ++ "\n"
-
-makeUweFileText :: [(String, String)] -> [String] -> String -> String -> String
-makeUweFileText imports fs close obj = importsStr ++ "\nmain = startRunAndCleanupProcess " ++ showStrList fs ++ " " ++ close ++ " $ objToIO infiniteDepth $ " ++ obj ++ "\n" where
-    importsStr = makeImportStatements allImports
+makeUweFileHelper :: [(String, String)] -> [String] -> String -> String -> FilePath -> IO FilePath
+makeUweFileHelper imports fs close obj = makeFile allImports mainText where
     allImports = imports ++ (makeUnqualifiedImportTups $ map ("Uwecode." ++) ["UweObj", "IO", "BasicUweObjs", "Conversion"])
+    mainText = "startRunAndCleanupProcess " ++ showStrList fs ++ " " ++ close ++ " $ objToIO infiniteDepth $ " ++ obj
 
-figureOutGoodFileName :: String -> IO String
-figureOutGoodFileName s = do
-    f1 <- findFile ["."] s
-    f2 <- findFile ["."] (s++".hs")
-    if isNothing f1 && isNothing f2 then return s else figureOutGoodFileName $ '_':s
-
-ioMaybeToMaybeTIO :: IO (Maybe a) -> MaybeT IO a
-ioMaybeToMaybeTIO io = do
-    m <- lift io
-    MaybeT $ return m
-
-checkForFail :: MaybeT IO a -> IO ()
-checkForFail mio = do
-    m <- runMaybeT mio
-    if isNothing m then hPutStr stderr "failed\n" else return ()
-
-makeUweFile :: FilePath -> MaybeT IO String
+makeUweFile :: FilePath -> MaybeT IO FilePath
 makeUweFile path = do
     (projImports, fs, close) <- lift $ getProjectIOs $ projectLocation path
     mainObj <- getMainObjFromFile path
     (objImports, objString) <- lift $ optimizeObj (projectLocation path) mainObj
-    fileName <- lift $ figureOutGoodFileName $ takeFileName $ dropExtension $ path
-    lift $ writeFile (fileName ++ ".hs") $ makeUweFileText (union projImports objImports) fs close objString
-    return fileName
+    lift $ makeUweFileHelper (union projImports objImports) fs close objString path
 
 buildUweFile :: FilePath -> MaybeT IO FilePath
 buildUweFile path = do
     tmpPath <- makeUweFile path
-    ghc <- ioMaybeToMaybeTIO $ findExecutable "ghc"
-    lift $ system $ ghc ++ " -no-keep-hi-files -no-keep-o-files " ++ tmpPath ++ ".hs" -- TODO look at exit code
-    lift $ system $ "rm " ++ (tmpPath ++ ".hs")
+    callGHC $ " -no-keep-hi-files -no-keep-o-files " ++ tmpPath ++ ".hs"
+    lift $ rmFile $ tmpPath ++ ".hs"
     return tmpPath
 
 runUweFile :: FilePath -> MaybeT IO ()
 runUweFile path = do
     tmpPath <- buildUweFile path
-    lift $ system $ "./" ++ tmpPath
-    lift $ system $ "rm " ++ tmpPath
+    lift $ runFile tmpPath
+    lift $ rmFile $ tmpPath
     return ()
 
 printError :: String -> IO ()
---
+++ Uwecode/Project/Util.hs
@@ -0,0 +1,57 @@
+module Uwecode.Project.Util where
+
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Maybe
+import System.IO
+import System.Process
+import System.Directory
+import Data.Maybe
+import Data.List
+
+showStrList :: [String] -> String
+showStrList s = '[' : (concat $ intersperse ", " s) ++ "]"
+
+makeImportStatements :: [(String, String)] -> String
+makeImportStatements = concat . map helper where
+    helper (i, "") = "import " ++ i ++ "\n"
+    helper (i, j)  = "import qualified " ++ i ++ " as " ++ j ++ "\n"
+
+figureOutGoodFileName :: FilePath -> IO FilePath
+figureOutGoodFileName s = do
+    f1 <- findFile ["."] s
+    f2 <- findFile ["."] (s++".hs")
+    if isNothing f1 && isNothing f2 then return s else figureOutGoodFileName $ '_':s
+
+ioMaybeToMaybeTIO :: IO (Maybe a) -> MaybeT IO a
+ioMaybeToMaybeTIO io = do
+    m <- lift io
+    MaybeT $ return m
+
+checkForFail :: MaybeT IO a -> IO ()
+checkForFail mio = do
+    m <- runMaybeT mio
+    if isNothing m then hPutStr stderr "failed\n" else return ()
+
+makeFileText :: [(String, String)] -> String -> String
+makeFileText imports mainText = makeImportStatements imports ++ "\nmain = " ++ mainText ++ "\n"
+
+makeFile :: [(String, String)] -> String -> FilePath -> IO FilePath
+makeFile imports mainText startPath = do
+    path <- figureOutGoodFileName startPath
+    writeFile (path ++ ".hs") $ makeFileText imports mainText
+    return path
+
+findGHC :: MaybeT IO FilePath
+findGHC = ioMaybeToMaybeTIO $ findExecutable "ghc"
+
+callGHC :: String -> MaybeT IO ()
+callGHC args = do
+    ghc <- findGHC
+    lift $ system $ ghc ++ args -- TODO look at exit code
+    return ()
+
+rmFile :: FilePath -> IO ()
+rmFile path = (system $ "rm " ++ path) >> return ()
+
+runFile :: FilePath -> IO ()
+runFile path = (system $ "./" ++ path) >> return ()

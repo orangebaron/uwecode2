2c0f5f3facfbf526265127b6bd701e644898c51b
Sam Alws <sam.alws@gmail.com>
2020-01-06 16:41:04 -0600

    finish project ios

--- Uwecode/Project/ProjectIOs.hs
+++ Uwecode/Project/ProjectIOs.hs
@@ -31,3 +31,34 @@ addIosImportIO obj = maybe unsuccessful (lift . addIosImport) $ do
     left <- ignoringConversion objToString Nothing leftObj
     right <- ignoringConversion objToString Nothing rightObj
     return (left, right)
+
+setIosCloserIO :: UweObj -> UweIOMonad ()
+setIosCloserIO = maybe unsuccessful (lift . setIosCloser) . ignoringConversion objToString Nothing
+
+readOptsFile :: IO ([(String, String)], [String])
+readOptsFile = do
+    text <- readFile $ optsLocation "."
+    return $ if text == "" then ([], []) else read text
+
+writeOptsFile :: ([(String, String)], [String]) -> IO ()
+writeOptsFile contents = writeFile (optsLocation ".") $ show contents
+
+addOptsImport :: (String, String) -> IO ()
+addOptsImport imp = do
+    (imps, opts) <- readOptsFile
+    writeOptsFile (imp : imps, opts)
+
+addOpt :: String -> IO ()
+addOpt opt = do
+    (imps, opts) <- readOptsFile
+    writeOptsFile (imps, opt : opts)
+
+addOptsImportIO :: UweObj -> UweIOMonad ()
+addOptsImportIO obj = maybe unsuccessful (lift . addOptsImport) $ do
+    (leftObj, rightObj) <- ignoringConversion objToTuple Nothing obj
+    left <- ignoringConversion objToString Nothing leftObj
+    right <- ignoringConversion objToString Nothing rightObj
+    return (left, right)
+
+addOptIO :: UweObj -> UweIOMonad ()
+addOptIO = maybe unsuccessful (lift . addOpt) .  ignoringConversion objToString Nothing
